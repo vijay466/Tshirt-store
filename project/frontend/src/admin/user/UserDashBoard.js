e53c771de009388e4ffcffe230f72d6d3247d435
vijay466 <65277424+vijay466@users.noreply.github.com>
2020-10-09 14:15:39 +0530

    Add files via upload

---
+++ project/frontend/src/admin/user/UserDashBoard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Base from './../core/Base';
+
+
+const UserDashboard = () => {
+    return (
+        <Base title= "User page">
+            <h1>Currently Unavailable please wait for the update</h1>
+        </Base>
+    )
+}
+export default UserDashboard;
